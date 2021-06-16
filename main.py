dbd0270b97fbeab666987c3902f5c602fcea33d7
nitomoki <nitomoki.2@gmail.com>
2021-06-16 16:50:18 +0900

    add: patch

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 import matplotlib.pyplot as plt
+import matplotlib.patches as patch
 import numpy as np
+import math
+from mpl_toolkits.mplot3d import Axes3D
+import mpl_toolkits.mplot3d.art3d as art3d
 
-x = np.linspace(0,1,100)
-y = x**2
+from scipy.stats import multivariate_normal
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.plot(x, y)
+
+
+fig = plt.figure(figsize=(8,8))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_title("Surface Plot", size=20)
+ax.set_xlabel("x", size = 14)
+ax.set_ylabel("y", size = 14)
+ax.set_zlabel("z", size = 14)
+ax.set_xticks([-1.0, -0.5, 0.0, 0.5, 1.0])
+ax.set_yticks([-1.0, -0.5, 0.0, 0.5, 1.0])
+
+line = art3d.Line3D([0,3],[0,3],[0,3], color='blue')
+ax.add_line(line)
+
+pi = np.pi
+n = 256
+
+t = np.linspace(-6*pi, 6*pi, n)
+
+x = np.cos(t)
+y = np.sin(t)
+z = t
+
+ax.plot(x, y, z, color="red")
 
 plt.show()
